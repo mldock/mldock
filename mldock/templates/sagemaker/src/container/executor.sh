032bc8d46262bf82c60d5a2c39b5e476ad2723e2
Sheldon Williams <sheldz.shakes.williams@gmail.com>
2021-06-21 06:14:34 +0200

    remove sm varient as default option

--- mldock/templates/sagemaker/src/container/executor.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-script_name="$1"
-shift
-if [ "$script_name" = "train" ]; then
-    python ./src/container/training/train.py $@
-else
-    python ./src/container/prediction/serve.py
-fi
