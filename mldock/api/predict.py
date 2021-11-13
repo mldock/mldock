58edbc87b9e22ea012c87f3ba893be7a59ef15b2
sheldon <sheldonw@ringier.co.za>
2021-11-13 18:05:30 +0200

    successful handling of csv, json, jpeg

--- mldock/api/predict.py
+++ mldock/api/predict.py
@@ -1,6 +1,7 @@
 """Predict Request API utilties"""
 import io
 import json
+import csv
 import requests
 
 def execute_request(url, headers, data):
@@ -11,10 +12,26 @@ def execute_request(url, headers, data):
         data=data
     )
 
-def send_json(filepath, host):
+def save_bytes_as_png(bytes_img, filepath):
+    """save bytes image as png throgh pillow"""
+    image = Image.frombytes('RGB', (512, 512), bytes_img)
+    image.save(filepath)
+
+def save_json(json_data, filepath):
+    """save json data to .json"""
+    with open(filepath, "w") as file_:
+        json.dump(json_data, file_)
+
+def save_csv(text_data, filepath):
+    """save text data as csv/txt"""
+    with open(filepath, "w") as file_:
+        writer = csv.writer(f)
+
+        for row in text_data:
+            writer.writerow(row)
+
+def send_json(data, host):
     """Send a json payload in request providing a filepath."""
-    with open(filepath, 'r') as file_:
-        data = json.load(file_)
 
     response = execute_request(
         url=host,
@@ -29,19 +46,10 @@ def send_json(filepath, host):
             )
         )
 
-    return "{}".format(
-        json.dumps(
-            response.json(),
-            indent=4,
-            separators=(',', ': '),
-            sort_keys=True
-        )
-    )
+    return response
 
-def send_csv(filepath, host):
+def send_csv(data, host):
     """Send a json payload in request providing a filepath."""
-    with open(filepath, 'r') as file_:
-        data = file_.read()
 
     response = execute_request(
         url=host,
@@ -56,18 +64,15 @@ def send_csv(filepath, host):
                 response.raise_for_status()
             )
         )
-    return response.text
+    return response
 
-def send_image_jpeg(filepath, host):
+def send_image_jpeg(data, host):
     """Send a image jpeg payload as bytes in request providing a filepath."""
-
-    with open(filepath, 'rb') as file_:
-        byte_content = io.BytesIO(file_.read())
 
     response = execute_request(
         url=host,
         headers={'Content-Type': 'image/jpeg'},
-        data=byte_content
+        data=data
     )
 
     if response.status_code != 200:
@@ -77,11 +82,82 @@ def send_image_jpeg(filepath, host):
                 response.raise_for_status()
             )
         )
-    return "{}".format(
-        json.dumps(
-            response.json(),
-            indent=4,
-            separators=(',', ': '),
-            sort_keys=True
-        )
+    return response
+
+def handle_request(host, file_path, content_type):
+
+    if content_type == 'application/json':
+
+        with open(file_path, 'r') as file_:
+            data = json.load(file_)
+
+        response_obj = send_json(data, host)
+        return response_obj
+
+    elif content_type == 'image/jpeg':
+
+        with open(file_path, 'rb') as file_:
+            data = io.BytesIO(file_.read())
+
+        response_obj = send_image_jpeg(data, host)
+
+        return response_obj
+
+    elif content_type == 'text/csv':
+
+        with open(file_path, 'r') as file_:
+            data = file_.read()
+
+        response_obj = send_csv(data, host)
+
+        return response_obj
+
+
+def handle_response(response, content_type, file_path=None):
+    """handle response. Extract, transform and emit/write to file"""
+
+    if content_type == 'application/json':
+
+        if file_path is None:
+            return response.json()
+
+        elif Path(file_path).is_file():
+            save_json(response.json(), file_path)
+
+    elif content_type == 'image/jpeg':
+
+        if file_path is None:
+            return response._content
+
+        elif Path(file_path).is_file():
+            save_bytes_as_png(response._content, file_path)
+
+    elif content_type == 'text/csv':
+
+        if file_path is None:
+            return response.text
+
+        elif Path(file_path).is_file():
+            save_csv(response.text, file_path)
+
+def handle_prediction(
+    host: str,
+    request: str,
+    response_file: str = None,
+    request_content_type: str = 'application/json',
+    response_content_type: str = 'application/json'
+):
+
+    # handle request (load to obj, convert, send, get response)
+    response_obj = handle_request(
+        host=host,
+        file_path=request,
+        content_type=request_content_type
+    )
+
+    # handle reponse (write file, print to terminal)
+    return handle_response(
+        response=response_obj,
+        content_type=response_content_type,
+        file_path=response_file
     )
--- tests/api/test_predict.py
+++ tests/api/test_predict.py
@@ -1,107 +1,162 @@
 """Test Predict API calls"""
 import io
+from PIL import Image
 from dataclasses import dataclass
 import pytest
 from mock import patch
-from mldock.api.predict import send_image_jpeg, send_csv, send_json
+from mldock.api.predict import send_image_jpeg, send_csv, send_json, handle_prediction
 import responses
 import requests
 
 @pytest.fixture
 def image_bytes():
     """reads image as bytes string"""
-    with open('tests/api/fixtures/eight.png', 'rb') as file_:
-        return io.BytesIO(file_.read())
+
+    img = Image.open('tests/api/fixtures/eight.png', mode='r')
+    img_byte_arr = io.BytesIO()
+    img.save(img_byte_arr, format='PNG')
+    return img_byte_arr.getvalue()
 
 @dataclass
 class MockResponse:
 
     status_code: int
     json_data: dict = None
-    text_data: str = None
+    text: str = None
+    _content: bytes = None
 
     def json(self):
         return self.json_data
-    
-    def text(self):
-        return self.text_data
 
 class TestPredictAPI:
 
     @staticmethod
     @responses.activate
-    def test_send_json_handles_non_200():
+    def test_handle_prediction_send_json_handles_non_200():
 
         responses.add(
             responses.POST, 'http://nothing-to-see-here/invocations',
-            json={'result': 'hello'},
+            json={'error': 'client error'},
             status=404
         )
         with pytest.raises(requests.exceptions.RequestException):
-            _ = send_json(
-                filepath="tests/api/fixtures/payload.json",
-                host="http://nothing-to-see-here/invocations"
+            _ = handle_prediction(
+                host="http://nothing-to-see-here/invocations",
+                request="tests/api/fixtures/payload.json",
+                response_file=None,
+                request_content_type='application/json',
+                response_content_type='application/json'
             )
 
     @staticmethod
-    def test_send_image_jpeg_request_gets_correct_input(image_bytes):
+    @responses.activate
+    def test_handle_prediction_sending_image_jpeg_handles_non_200():
 
-
-        with patch('mldock.api.predict.execute_request') as mock_execute_request:
-            mock_execute_request.return_value = MockResponse(json_data={"result": "hello"}, status_code=200)
-            result = send_image_jpeg(
-                filepath="tests/api/fixtures/eight.png",
-                host="http://nothing-to-see-here/invocations"
+        responses.add(
+            responses.POST, 'http://nothing-to-see-here/invocations',
+            json={'error': 'client error'},
+            status=404
+        )
+        with pytest.raises(requests.exceptions.RequestException):
+            _ = handle_prediction(
+                host="http://nothing-to-see-here/invocations",
+                request="tests/api/fixtures/eight.png",
+                response_file=None,
+                request_content_type='image/jpeg',
+                response_content_type='application/json'
             )
-        
-        validation_kwargs = {
-            'url': 'http://nothing-to-see-here/invocations',
-            'headers': {'Content-Type': 'image/jpeg'}
-        }
-        _, kwargs = list(mock_execute_request.call_args)
-
-        data_obj = kwargs.pop('data')
-        assert kwargs == validation_kwargs, "Failure. URL and Headers are incorrect."
-        assert isinstance(data_obj, io.BytesIO), "Failure. Expected io.BytesIO object."
 
     @staticmethod
-    def test_send_json_request_gets_correct_input(image_bytes):
+    @responses.activate
+    def test_handle_prediction_sending_text_csv_handles_non_200():
 
-
-        with patch('mldock.api.predict.execute_request') as mock_execute_request:
-            mock_execute_request.return_value = MockResponse(json_data={"result": "hello"}, status_code=200)
-            result = send_json(
-                filepath="tests/api/fixtures/payload.json",
-                host="http://nothing-to-see-here/invocations"
+        responses.add(
+            responses.POST, 'http://nothing-to-see-here/invocations',
+            json={'error': 'client error'},
+            status=404
+        )
+        with pytest.raises(requests.exceptions.RequestException):
+            _ = handle_prediction(
+                host="http://nothing-to-see-here/invocations",
+                request="tests/api/fixtures/payload.csv",
+                response_file=None,
+                request_content_type='text/csv',
+                response_content_type='application/json'
             )
-        
-        validation_kwargs = {
-            'url': 'http://nothing-to-see-here/invocations',
-            'headers': {'Content-Type': 'application/json'}
-        }
-        _, kwargs = list(mock_execute_request.call_args)
-
-        data_obj = kwargs.pop('data')
-        assert kwargs == validation_kwargs, "Failure. URL and Headers are incorrect."
-        assert isinstance(data_obj, str), "Failure. Expected str json object."
 
     @staticmethod
-    def test_send_csv_request_gets_correct_input(image_bytes):
+    def test_handle_prediction_send_json_success_200():
 
-
-        with patch('mldock.api.predict.execute_request') as mock_execute_request:
-            mock_execute_request.return_value = MockResponse(text_data='result\nhello\n', status_code=200)
-            result = send_csv(
-                filepath="tests/api/fixtures/payload.csv",
-                host="http://nothing-to-see-here/invocations"
+       with patch('mldock.api.predict.execute_request') as mock_execute_request:
+            mock_execute_request.return_value = MockResponse(
+                json_data={'result': 'success'},
+                status_code=200
             )
-        
-        validation_kwargs = {
-            'url': 'http://nothing-to-see-here/invocations',
-            'headers': {'Content-Type': 'text/csv'}
-        }
-        _, kwargs = list(mock_execute_request.call_args)
+            _ = handle_prediction(
+                host="http://nothing-to-see-here/invocations",
+                request="tests/api/fixtures/payload.json",
+                response_file=None,
+                request_content_type='application/json',
+                response_content_type='application/json'
+            )
 
-        data_obj = kwargs.pop('data')
-        assert kwargs == validation_kwargs, "Failure. URL and Headers are incorrect."
-        assert isinstance(data_obj, str), "Failure. Expected str json object."
+            validation_kwargs = {
+                'url': 'http://nothing-to-see-here/invocations',
+                'headers': {'Content-Type': 'application/json'}
+            }
+            _, kwargs = list(mock_execute_request.call_args)
+
+            data_obj = kwargs.pop('data')
+            assert kwargs == validation_kwargs, "Failure. URL and Headers are incorrect."
+            assert isinstance(data_obj, str), "Failure. Expected str json object."
+
+    @staticmethod
+    def test_handle_prediction_sending_image_jpeg_success_200(image_bytes):
+
+       with patch('mldock.api.predict.execute_request') as mock_execute_request:
+            mock_execute_request.return_value = MockResponse(
+                _content=image_bytes,
+                status_code=200
+            )
+            _ = handle_prediction(
+                host="http://nothing-to-see-here/invocations",
+                request="tests/api/fixtures/eight.png",
+                response_file=None,
+                request_content_type='image/jpeg',
+                response_content_type='image/jpeg'
+            )
+            validation_kwargs = {
+                'url': 'http://nothing-to-see-here/invocations',
+                'headers': {'Content-Type': 'image/jpeg'}
+            }
+            _, kwargs = list(mock_execute_request.call_args)
+
+            data_obj = kwargs.pop('data')
+            assert kwargs == validation_kwargs, "Failure. URL and Headers are incorrect."
+            assert isinstance(data_obj, io.BytesIO), "Failure. Expected io.BytesIO object."
+
+    @staticmethod
+    def test_handle_prediction_sending_text_csv_success_200():
+
+       with patch('mldock.api.predict.execute_request') as mock_execute_request:
+            mock_execute_request.return_value = MockResponse(
+                text="greet,name\nhello,sam",
+                status_code=200
+            )
+            _ = handle_prediction(
+                host="http://nothing-to-see-here/invocations",
+                request="tests/api/fixtures/payload.csv",
+                response_file=None,
+                request_content_type='text/csv',
+                response_content_type='text/csv'
+            )
+
+            validation_kwargs = {
+                'url': 'http://nothing-to-see-here/invocations',
+                'headers': {'Content-Type': 'text/csv'}
+            }
+            _, kwargs = list(mock_execute_request.call_args)
+
+            data_obj = kwargs.pop('data')
+            assert kwargs == validation_kwargs, "Failure. URL and Headers are incorrect."
+            assert isinstance(data_obj, str), "Failure. Expected str json object."
